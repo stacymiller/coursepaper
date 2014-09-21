06c5ec98fb2949a285c517e9a3fa06f44baebcb3
stacymiller <stacymiller.pml30@gmail.com>
2014-09-22 00:35:20 +0400

    added imitation of asset's random behavior

---
+++ app/src/Main.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+import java.util.Random;
+
+class ImitatedAsset{
+    double price;
+    ImitatedAsset[] children;
+    boolean lastChild;
+
+    ImitatedAsset(double newPrice, int branches){
+        price = newPrice;
+        children = new ImitatedAsset[branches];
+        lastChild = true;
+    }
+
+    ImitatedAsset(double newPrice, int branches, boolean isLastChild){
+        price = newPrice;
+        children = new ImitatedAsset[branches];
+        lastChild = isLastChild;
+    }
+
+    public String toString(){
+        String ans = "";
+        if (!lastChild) {
+            for (ImitatedAsset child: children){
+                ans = ans + child.toString() + "\n";
+            }
+            ans = ans.trim() + "\t";
+        }
+        ans = ans + price + "\n";
+        return ans;
+    }
+}
+
+public class Main {
+    static Random rnd = new Random();
+    static double lambda = 10.;
+
+    public static ImitatedAsset generateAssets(int branches, int steps, double initialPrice){
+        ImitatedAsset ans = new ImitatedAsset(initialPrice, branches, steps == 0);
+        if (steps > 0) {
+            for (int branch = 0; branch < branches; branch++) {
+                double price = initialPrice + rnd.nextGaussian() * lambda;
+//                System.out.println(((Double)price).toString() + " " + ((Integer)steps).toString());
+                ans.children[branch] = generateAssets(branches, steps-1, price);
+//                System.out.println(price);
+            }
+//            System.out.println(Arrays.toString(ans.children));
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int branches = 3;
+        int steps = 5;
+        double initialPrice = 100.;
+        ImitatedAsset ia = generateAssets(branches, steps, initialPrice);
+        System.out.println(ia);
+        System.out.println("Hello World!");
+    }
+}
