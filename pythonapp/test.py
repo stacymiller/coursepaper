c27aaa165093a83ae5733cf774bf553f3a965c5d
Anastasia Miller <stacymiller.pml30@gmail.com>
2015-05-18 11:21:24 +0300

    collecting stats

--- pythonapp/test.py
+++ pythonapp/test.py
@@ -1,36 +1,47 @@
-import numpy
+import numpy as np
 from numpy.core.umath import sqrt
+from scipy.stats import norm
 import pylab
+import matplotlib
 
 __author__ = 'stacymiller'
 
 from scipy.stats.distributions import norm
+
 b = 10
-w = [1./b for i in xrange(b)]
-mu = 0.3
-sigma = 0.1
+# w = [1. / b for i in xrange(b)]
+mu = 0.2
+sigma = 0.005
 deltat = 0.05
-xnext = lambda xprev: [x*(1 + mu*deltat + sigma*sqrt(deltat)*norm.rvs()) for i in xrange(b) for x in xprev]
-def generator(branches, parents):
-    x = []
-    for p in parents:
-        x.extend(p*(1.+mu*deltat) + sigma*sqrt(deltat)*p*norm.rvs(size=branches))
-    return numpy.asarray(x)
 
 
-pylab.hist(generator(300, xnext((100, ))), bins=100)
-# print xnext((100, 200))
-x = xnext(xnext((100, )))
-param = dict(loc=x[0]*(1.+mu*deltat), scale=sigma*sqrt(deltat)*x[0])
-def F(r):
-    F_value = lambda a: sum([norm(loc=p*(1.+mu*deltat), scale=sigma*sqrt(deltat)*p).cdf(a) for p in x]) / (b*b)
-    # F_value = lambda a: norm(loc=x[0]*(1.+mu*deltat), scale=sigma*sqrt(deltat)*x[0]).cdf(a)
-    try:
-        return [F_value(arg) for arg in r]
-    except TypeError as te:
-        return F_value(r)
-# F = lambda r: [sum([norm(loc=p*(1.+mu*deltat), scale=sigma*sqrt(deltat)*p).cdf(arg) for p in x]) / b for arg in r] if len(r)
-dots = numpy.linspace(50,150,200)
-# pylab.plot(dots,F(dots),'ro')
-# pylab.grid(b=1)
+def GBM_consecutive(N, volatility, u, S0):
+    Wt = norm.rvs(size=N)
+    dt = 1. / N
+    St = np.empty((N, ))
+    St[0] = S0
+    for i in xrange(1, len(St)):
+        St[i] = St[i - 1] * np.exp((u - 0.5 * volatility * volatility) * dt + volatility * np.sqrt(dt) * Wt[i])
+    return St
+
+
+def GBM(N, volatility, u, S0):
+    Wt = np.cumsum(norm.rvs(size=N))
+    t = np.array(range(N)) / 365.
+    p1 = (u - 0.5 * volatility * volatility) * t
+    p2 = volatility * Wt
+    return S0 * np.exp(p1 + p2)
+
+
+font = {'family': 'normal',
+        'weight': 'normal',
+        'size': 22}
+matplotlib.rc('font', **font)
+
+asymptotic = lambda m, k: ((m + k + 1) ** (m + k + 1)) / (sqrt(2 * np.pi * k * (m + 1)) * (k ** k) * ((m + 1) ** (m + 1)))
+tr = map(asymptotic, xrange(4, 52), np.full((52-4, ), 10))
+pylab.plot(tr, linewidth=2)
+
+pylab.plot([0, 100], [110, 110], ls="-", linewidth=2, color="black")
+pylab.savefig("traces.eps", format="eps")
 pylab.show()
