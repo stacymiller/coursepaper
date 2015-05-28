0c19fa9c7bd564632c933580a133d08c8d7606f8
Anastasia Miller <anastasia.miller@jetbrains.com>
2015-05-28 07:15:04 +0300

    collecting stats

--- app/src/Main.java
+++ app/src/Main.java
@@ -12,17 +12,22 @@ public class Main {
         int sectors = 7;
         double initialPrice = 100.;
 
-        testConvergenceToTrueValue(4, 100);
+//        testConvergenceToTrueValue(4, 100);
+        testConvergenceToAmericanOption(100, 30);
     }
 
     private static String testConvergenceToTrueValue(int n, double initialPrice) throws FileNotFoundException, UnsupportedEncodingException {
-        String filename = "test_convergence_to_true_value_random_subtree.txt";
+        String filename = "test_convergence_to_true_value_random_subtree_modified_ev_2.txt";
         PrintWriter writer = new PrintWriter(filename, "UTF-8");
         writer.println("branches,upper_estimator,lower_estimator");
-        for (int branches = 20; branches < 5000; branches+=20) {
+        for (int branches = 5; branches < 500; branches+=5) {
             try {
-                for (int sample = 0; sample < 10000; sample++) {
+                for (int sample = 0; sample < 100; sample++) {
                     double[] ans = RandomSubtreeGeneratorEstimator.calculate(branches, n, initialPrice, 1.3*initialPrice);
+//                    ImitatedAsset ia = AssetGenerator.generateAssetTree(branches, n, initialPrice);
+//                    double[] ans = new double[]{BroadieGlassermanEstimation.upperEstimate(ia, 1.3*initialPrice),
+//                                                BroadieGlassermanEstimation.lowerEstimate(ia, 1.3*initialPrice)};
+//                    ia = null;
                     writer.println(String.format(Locale.ENGLISH, "%d, %f, %f", branches, ans[0], ans[1]));
                 }
             } catch (OutOfMemoryError oom) {
@@ -34,21 +39,18 @@ public class Main {
     }
 
     private static String testConvergenceToAmericanOption(double initialPrice, int branches) throws FileNotFoundException, UnsupportedEncodingException {
-        String filename = "test_convergence_to_american_option.txt";
-        double ansUp;
-        double ansLow;
+        String filename = "test_convergence_to_american_option_random_subtree.txt";
         PrintWriter writer = new PrintWriter(filename, "UTF-8");
         writer.println("execution_times,upper_estimator,lower_estimator");
         for (int n = 4; n < 1000; n++) {
             try {
                 for (int sample = 0; sample < 100; sample++) {
-                    ImitatedAsset ia;
-                    ia = EmpiricDistribAssetGenerator.generateAssetTree(branches, n, initialPrice);
-                    ansUp = BroadieGlassermanEstimation.upperEstimate(ia, 1.3 * initialPrice);
-                    ansLow = BroadieGlassermanEstimation.lowerEstimate(ia, 1.3 * initialPrice);
-                    writer.println(String.format(Locale.ENGLISH, "%d, %f, %f", n, ansUp, ansLow));
-                    System.out.println(String.format(Locale.ENGLISH, "%d, %f, %f", n, ansUp, ansLow));
-                    ia = null;
+                    double[] ans = RandomSubtreeGeneratorEstimator.calculate(branches, n, initialPrice, 1.3*initialPrice);
+//                    ImitatedAsset ia = AssetGenerator.generateAssetTree(branches, n, initialPrice);
+//                    double[] ans = new double[]{BroadieGlassermanEstimation.upperEstimate(ia, 1.3*initialPrice),
+//                                                BroadieGlassermanEstimation.lowerEstimate(ia, 1.3*initialPrice)};
+//                    ia = null;
+                    writer.println(String.format(Locale.ENGLISH, "%d, %f, %f", n, ans[0], ans[1]));
                 }
             } catch (OutOfMemoryError oom) {
                 break;
